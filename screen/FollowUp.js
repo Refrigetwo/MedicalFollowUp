21eab6f37856197e8b69374886e8b78df37dbeb1
Sean <ljx2391273@gmail.com>
2019-06-04 21:13:47 +0800

    随访模块输入页面完成

--- screen/FollowUp.js
+++ screen/FollowUp.js
@@ -1,5 +1,15 @@
 import React from 'react';
-import {Text, View, StyleSheet, Dimensions, TimePickerAndroid, TouchableHighlight, TouchableOpacity} from 'react-native';
+import {
+    Text,
+    View,
+    StyleSheet,
+    Dimensions,
+    TimePickerAndroid,
+    TouchableHighlight,
+    TouchableOpacity,
+    Button,
+    TextInput
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import Mail from './Mail';
 
@@ -10,10 +20,16 @@ export default class FollowUp extends React.Component {
             hour1:'00',
             minute1:'00',
             hour2:'00',
-            minute2:'00'
+            minute2:'00',
+            date:'',
+            highpre:120,
+            lowpre:90,
+            mass:60,
+            heart:73,
         };
         this._onPress1=this._onPress1.bind(this);
         this._onPress2=this._onPress2.bind(this);
+        this.save=this.save.bind(this);
     };
     static navigationOptions={
         header:null,
@@ -42,22 +58,62 @@ export default class FollowUp extends React.Component {
             })
     }
 
+    getDate(){
+        let date = new Date();
+        let month = date.getMonth() + 1;
+        month = month < 10 ? ('0' + month) : month;
+        let d = date.getDate();
+        d = d < 10 ? ('0' + d) : d;
+        return month+d;
+    }
+
+    save(){
+        var request_url='http://10.0.2.2:3000/info/add?date=2019-06-07&highpre='+this.state.highpre+'&lowpre='+this.state.lowpre+'&mass='+this.state.mass+'&heart='+this.state.heart+'&sleep0h='+this.state.hour1+'&sleep0m='+this.state.minute1+'&sleep1h='+this.state.hour2+'&sleep1m='+this.state.minute2+'&user_id=2';
+        fetch(request_url, {
+            method: 'GET',
+        });
+    }
+
     render(){
         return (
            <View style={styles.container}>
                <View style={styles.normalcontainer}>
                    <Text style={styles.text}>血压</Text>
-                   <Text style={styles.text}>100 - 130</Text>
+                   <View style={{flexDirection: 'row'}}>
+                        <TextInput
+                            style={{width:70}}
+                            onChangeText={(text) => this.setState({highpre:text})}
+                            placeholder={'高压'}
+                            placeholderTextColor={'#6b6e72'}
+                        />
+                        <Text style={styles.text}>-</Text>
+                        <TextInput
+                            style={{width:70}}
+                            onChangeText={(text) => this.setState({lowpre:text})}
+                            placeholder={'低压'}
+                            placeholderTextColor={'#6b6e72'}
+                        />
+                   </View>
                </View>
                <View style={styles.line}/>
                <View style={styles.normalcontainer}>
                    <Text style={styles.text}>心率</Text>
-                   <Text style={styles.text}>100</Text>
+                   <TextInput
+                       style={{width:70}}
+                       onChangeText={(text) => this.setState({heart:text})}
+                       placeholder={'心率'}
+                       placeholderTextColor={'#6b6e72'}
+                   />
                </View>
                <View style={styles.line}/>
                <View style={styles.normalcontainer}>
                    <Text style={styles.text}>体重</Text>
-                   <Text style={styles.text}>100</Text>
+                   <TextInput
+                       style={{width:70}}
+                       onChangeText={(text) => this.setState({mass:text})}
+                       placeholder={'体重'}
+                       placeholderTextColor={'#6b6e72'}
+                   />
                </View>
                <View style={styles.line}/>
                <View style={styles.normalcontainer}>
@@ -76,7 +132,12 @@ export default class FollowUp extends React.Component {
                <TouchableOpacity style={styles.floatbutton} onPress={()=>this.props.navigation.navigate('Mail')}>
                    <Icon name="mail"  size={30} color="#01a699" />
                </TouchableOpacity>
+               <View style={{height:100,backgroundColor:'#b5b5b5'}}/>
+               <Button title='保存'
+                       onPress={this.save}
+               />
            </View>
+
 
         );
     }
